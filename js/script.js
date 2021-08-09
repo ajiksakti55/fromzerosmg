0807a17c52ef25ab01182a8b89177ef9dfe3ad1a
ajiksakti55 <ajiksakti55@gmail.com>
2021-08-09 10:31:23 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 $(document).ready(function(){
     $(window).scroll(function(){
+        // sticky navbar on scroll script
         if(this.scrollY > 20){
             $('.navbar').addClass("sticky");
         }else{
             $('.navbar').removeClass("sticky");
         }
+        
+        // scroll-up button show/hide script
         if(this.scrollY > 500){
             $('.scroll-up-btn').addClass("show");
         }else{
@@ -12,32 +15,37 @@ $(document).ready(function(){
         }
     });
 
-    // slide up scripr
+    // slide-up script
     $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop:0});
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
     });
 
-    // saklar navbar
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
 
-
-    // typing bro
+    // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Pemasangan CCTV","Perbaikan CCTV","Perawatan CCTV"],
+        strings: ["Perbaikan", "Instalasi", "Perawatan", "CCTV System"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
-
-
-    // owl carosel
+    // owl carousel script
     $('.carousel').owlCarousel({
         margin: 20,
         loop: true,
+        autoplay: true,
         autoplayTimeOut: 2000,
         autoplayHoverPause: true,
         responsive: {
