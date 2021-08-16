e6e0197eafae3d00fa481d0bb691267b697bfec9
ajiksakti55 <ajiksakti55@gmail.com>
2021-08-16 10:15:41 +0700

    gads

--- script/script.js
+++ script/script.js
@@ -41,13 +41,6 @@ $(document).ready(function(){
         loop: true
     });
 
-    var typed = new Typed(".typing2", {
-        strings: ["cv pancamuda amerta"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-
 
     // owl carousel script
     $('.carousel').owlCarousel({
